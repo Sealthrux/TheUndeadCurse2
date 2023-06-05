8144291a078d2a236fd3ed81f7ee1ca59562c0ed
Just Seal <44335975+Sealthrux@users.noreply.github.com>
2023-06-05 15:30:57 +0100

    HUD 1

---
+++ The Undead Curse/Assets/Scripts/Objective.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Objective : MonoBehaviour
+{
+    public string Text1;
+    public string Text2;
+    public TMP_Text ObjectiveText;
+    public TMP_Text ObjectiveCounter;
+    public float ObjectiveNum;
+    public float CurrentObjectiveNum = 0;
+
+
+    public void Start()
+    {
+        // Create a temporary reference to the current scene.
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // Retrieve the name of this scene.
+        string sceneName = currentScene.name;
+
+
+        Text1 = "Consume"+ ObjectiveNum + "human meat";
+        Text2 = CurrentObjectiveNum + "/" + ObjectiveNum;
+
+        ObjectiveText.text = Text1;
+        ObjectiveCounter.text = Text2;
+
+        if (sceneName == "MainScene")
+        {
+            ObjectiveNum = 3;
+        }
+    }
+    public void Update()
+    {
+        
+    }
+}
